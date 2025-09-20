2e84735a87b590291bb7f8ca1a400cde6f4b8335
lolmageap <ekxk1234@naver.com>
2025-09-20 19:02:19 +0900

    add additional analyzers and normalizers for Elasticsearch configuration

--- src/main/kotlin/org/cherhy/sasuke/config/constant/Analyzer.kt
+++ src/main/kotlin/org/cherhy/sasuke/config/constant/Analyzer.kt
@@ -1,5 +1,17 @@
 package org.cherhy.sasuke.config.constant
 
 object Analyzer {
+    const val STANDARD_ANALYZER = "standard"
+    const val SIMPLE_ANALYZER = "simple"
     const val KOREAN_ANALYZER = "nori"
+    const val WHITESPACE_ANALYZER = "whitespace"
+    const val STOP_ANALYZER = "stop"
+    const val KEYWORD_ANALYZER = "keyword"
+    const val PATTERN_ANALYZER = "pattern"
+    const val LANGUAGE_ANALYZER = "language"
+    const val FINGERPRINT_ANALYZER = "fingerprint"
+
+    object NORMALIZER {
+        const val LOWERCASE_NORMALIZER = "lowercase_normalizer"
+    }
 }
