99baaacecb8bf83c699302ff8b06c33679474c3a
lolmageap <ekxk1234@naver.com>
2025-09-29 00:07:27 +0900

    add DocumentIdGenerator to create UUIDs for Elasticsearch Document IDs

--- src/main/kotlin/org/cherhy/sasuke/common/constant/DocumentId.kt
+++ src/main/kotlin/org/cherhy/sasuke/common/constant/DocumentId.kt
@@ -9,7 +9,7 @@ object DocumentId {
         "Elasticsearch Document ID는 영문 대소문자, 숫자, '-', '_' 문자로만 이루어져야 합니다. ${VALIDATE_VALUE}는 허용되지 않는 ID 형식입니다."
     )
 
-    private const val MAX_LENGTH: Int = 64
+    const val MAX_LENGTH: Int = 64
     val REGEX: Regex = "^[a-zA-Z0-9_-]{1,$MAX_LENGTH}$".toRegex()
 
     @JvmInline
@@ -19,5 +19,5 @@ object DocumentId {
 }
 
 object DocumentIdGenerator {
-    fun generate(): String = UUID.randomUUID().toString().replace("-", "")
+    fun generate(): String = UUID.randomUUID().toString().substring(0, DocumentId.MAX_LENGTH)
 }
