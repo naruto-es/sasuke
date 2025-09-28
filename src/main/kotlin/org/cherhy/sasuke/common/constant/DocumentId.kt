5e17d113ea873bb79f87a8e86d8e5d6b7751c66c
lolmageap <ekxk1234@naver.com>
2025-09-29 00:05:11 +0900

    add DocumentIdGenerator to create unique Document IDs using UUID

--- src/main/kotlin/org/cherhy/sasuke/common/constant/DocumentId.kt
+++ src/main/kotlin/org/cherhy/sasuke/common/constant/DocumentId.kt
@@ -1,5 +1,7 @@
 package org.cherhy.sasuke.common.constant
 
+import java.util.UUID
+
 object DocumentId {
     const val NAME: String = "_id"
     const val VALIDATE_VALUE: String = "{value}"
@@ -14,4 +16,8 @@ object DocumentId {
     value class DocumentValidateMessage(val message: String) {
         fun format(value: String): String = this.message.replace(VALIDATE_VALUE, value)
     }
+}
+
+object DocumentIdGenerator {
+    fun generate(): String = UUID.randomUUID().toString().replace("-", "")
 }
