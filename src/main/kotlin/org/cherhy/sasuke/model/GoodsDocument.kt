26bcef88fdfb1cdfad51c4dd456f3fdd987d0d7c
lolmageap <ekxk1234@naver.com>
2025-09-28 23:47:46 +0900

    refactor GoodsDocumentId to use String type and add validation for Elasticsearch Document ID format

--- src/main/kotlin/org/cherhy/sasuke/model/GoodsDocument.kt
+++ src/main/kotlin/org/cherhy/sasuke/model/GoodsDocument.kt
@@ -1,5 +1,6 @@
 package org.cherhy.sasuke.model
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import org.cherhy.sasuke.config.constant.Analyzer.KOREAN_ANALYZER
 import org.springframework.data.annotation.Id
 import org.springframework.data.elasticsearch.annotations.Document
@@ -8,12 +9,26 @@ import org.springframework.data.elasticsearch.annotations.FieldType
 import java.math.BigDecimal
 
 @JvmInline
-value class GoodsDocumentId(val value: Long)
+value class GoodsDocumentId private constructor(val value: String) {
+    init {
+        require(value.matches(REGEX)) { "${value}는 Elasticsearch Document ID로 허용되지 않는 ID 형식입니다." }
+    }
+
+    operator fun invoke(value: String) = of(value)
+
+    companion object {
+        private val REGEX = "^[a-zA-Z0-9_-]{1,64}$".toRegex()
+
+        @JvmStatic
+        @JsonCreator
+        fun of(value: String) = GoodsDocumentId(value.trim())
+    }
+}
 
 @Document(indexName = "goods")
 data class GoodsDocument(
     @Id
-    @field:Field(type = FieldType.Long)
+    @field:Field(type = FieldType.Keyword, name = "_id")
     val id: GoodsDocumentId,
 
     @field:Field(type = FieldType.Keyword)
