00e8b23137665d26b290a9140a348ff5949adf8a
lolmageap <ekxk1234@naver.com>
2025-10-07 22:31:47 +0900

    refactor ElasticSearchBuilder to use rootCriteria for query construction

--- src/main/kotlin/org/cherhy/sasuke/common/extension/ElasticsearchRepository.kt
+++ src/main/kotlin/org/cherhy/sasuke/common/extension/ElasticsearchRepository.kt
@@ -1,16 +1,29 @@
 package org.cherhy.sasuke.common.extension
 
 import org.cherhy.sasuke.dsl.ElasticSearchBuilder
+import org.springframework.data.domain.Pageable
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations
 import org.springframework.data.elasticsearch.core.query.CriteriaQuery
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 
-inline fun <reified T, R> ElasticsearchRepository<T, R>.query(
+inline fun <reified T, R> ElasticsearchOperations.query(
     elasticSearchBuilder: ElasticSearchBuilder,
-    operations: ElasticsearchOperations,
 ): SearchResponse<T> {
-    val query = CriteriaQuery(elasticSearchBuilder.criteria)
-    val searchHits = operations.search(query, T::class.java)
+    val query = CriteriaQuery(elasticSearchBuilder.rootCriteria)
+    val searchHits = this.search(query, T::class.java)
+    val contents: List<T> = searchHits.searchHits.map { it.content }
+
+    return SearchResponse(
+        contents = contents,
+        totalHits = searchHits.totalHits,
+    )
+}
+
+inline fun <reified T, R> ElasticsearchOperations.query(
+    elasticSearchBuilder: ElasticSearchBuilder,
+    pageable: Pageable,
+): SearchResponse<T> {
+    val query = CriteriaQuery(elasticSearchBuilder.rootCriteria).setPageable<CriteriaQuery>(pageable)
+    val searchHits = this.search(query, T::class.java)
     val contents: List<T> = searchHits.searchHits.map { it.content }
 
     return SearchResponse(
--- src/main/kotlin/org/cherhy/sasuke/dsl/ElasticSearchBuilder.kt
+++ src/main/kotlin/org/cherhy/sasuke/dsl/ElasticSearchBuilder.kt
@@ -9,59 +9,63 @@ annotation class ElasticSearchDSL
 
 @ElasticSearchDSL
 class ElasticSearchBuilder {
-    val criteria = Criteria()
+    val rootCriteria = Criteria()
 
     infix fun <T : Any, R> KProperty1<T, R>.match(value: R) {
         val newCriteria = Criteria(this.name).`is`(value as Any)
-        appendCriteria(newCriteria)
+        rootCriteria.and(newCriteria)
     }
 
     infix fun <T : Any> KProperty1<T, Int>.range(range: IntRange) {
         val newCriteria = Criteria(this.name)
             .greaterThanEqual(range.first)
             .lessThanEqual(range.last)
-        appendCriteria(newCriteria)
+        rootCriteria.and(newCriteria)
     }
 
     infix fun <T : Any, R> KProperty1<T, R>.term(value: R) {
-        appendCriteria(
+        rootCriteria.and(
             Criteria(this.name).`is`(value as Any)
         )
     }
 
     infix fun <T : Any, R> KProperty1<T, R>.gt(value: R) where R : Comparable<R> {
-        appendCriteria(Criteria(this.name).greaterThan(value))
+        rootCriteria.and(
+            Criteria(this.name).greaterThan(value)
+        )
     }
 
     infix fun <T : Any, R> KProperty1<T, R>.gte(value: R) where R : Comparable<R> {
-        appendCriteria(Criteria(this.name).greaterThanEqual(value))
+        rootCriteria.and(
+            Criteria(this.name).greaterThanEqual(value)
+        )
     }
 
     infix fun <T : Any, R> KProperty1<T, R>.lt(value: R) where R : Comparable<R> {
-        appendCriteria(Criteria(this.name).lessThan(value))
+        rootCriteria.and(
+            Criteria(this.name).lessThan(value)
+        )
     }
 
     infix fun <T : Any, R> KProperty1<T, R>.lte(value: R) where R : Comparable<R> {
-        appendCriteria(Criteria(this.name).lessThanEqual(value))
+        rootCriteria.and(
+            Criteria(this.name).lessThanEqual(value)
+        )
     }
 
     infix fun and(block: ElasticSearchBuilder.() -> Unit) {
         val nested = ElasticSearchBuilder().apply(block)
-        criteria.and(nested.criteria)
+        rootCriteria.and(nested.rootCriteria)
     }
 
     infix fun or(block: ElasticSearchBuilder.() -> Unit) {
         val nested = ElasticSearchBuilder().apply(block)
-        criteria.or(nested.criteria)
-    }
-
-    private fun appendCriteria(criteria: Criteria) {
-        criteria.and(criteria)
+        rootCriteria.or(nested.rootCriteria)
     }
 }
 
 fun elasticSearch(block: ElasticSearchBuilder.() -> Unit): CriteriaQuery {
     val builder = ElasticSearchBuilder()
     builder.block()
-    return CriteriaQuery(builder.criteria)
+    return CriteriaQuery(builder.rootCriteria)
 }
