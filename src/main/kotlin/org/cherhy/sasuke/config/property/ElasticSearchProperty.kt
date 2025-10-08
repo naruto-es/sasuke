aea2db22b7893a819fac9e16bc8f9944054a953f
lolmageap <ekxk1234@naver.com>
2025-10-08 23:14:56 +0900

    update MySQL datasource URL to allow public key retrieval and change ElasticSearchProperty fields to public

--- src/main/kotlin/org/cherhy/sasuke/config/property/ElasticSearchProperty.kt
+++ src/main/kotlin/org/cherhy/sasuke/config/property/ElasticSearchProperty.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "elasticsearch")
 data class ElasticSearchProperty(
-    private val host: String,
-    private val port: Int,
+    val host: String,
+    val port: Int,
 ) {
     val uri: String
         get() = "$host:$port"
